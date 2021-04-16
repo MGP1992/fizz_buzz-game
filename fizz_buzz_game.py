17651075aa96e1d7f9cbbad0c223e19e2b9d6fc2
MGP1992 <82448619+MGP1992@users.noreply.github.com>
2021-04-16 09:59:30 +0100

    Add files via upload

---
+++ fizz_buzz_game.py
@@ -0,0 +1,33 @@
+def fizz_buzz(number: int) -> str:
+
+    if number % 15 == 0:
+        return "fizz buzz"
+    elif number % 3 == 0:
+        return "fizz"
+    elif number % 5 == 0:
+        return "buzz"
+    return str(number)
+highest = int(input("Please select the number you wish to play to: "))
+next_number = 0
+while next_number < highest - 1:
+    next_number += 1
+    print(fizz_buzz(next_number))
+    next_number += 1
+    correct_answer = fizz_buzz(next_number)
+    player_answer = input("Enter your number: ")
+    if correct_answer == player_answer:
+        print("Well done! You got it!")
+    else:
+        print("I'm sorry that was incorrect, you lose :(")
+        break
+
+
+
+
+
+
+
+
+
+
+
